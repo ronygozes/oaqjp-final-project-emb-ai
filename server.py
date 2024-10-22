f19b91eb132312222ec9b3092c0bec9689f56f71
rony gozes <ronygozes@gmail.com>
2024-10-21 21:01:27 -0400

    done with the server, static code analysis, and the project

--- server.py
+++ server.py
@@ -1,25 +1,27 @@
+"""A server of emotion detection application"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
+    """render home page for application"""
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def detect_emotions():
+    """analyze input data and determine the relevant emotions"""
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
     if emotions['dominant_emotion'] is None:
         response = 'Invalid text! Please try again!'
     else:
         emotions_list = list(emotions.items())
-        for em in emotions_list:
+        for em in emotions_list.copy():
             if em[0] == 'dominant_emotion':
                 emotions_list.remove(em)
                 break
-        response = f"For the given statement, the system response is "
+        response = "For the given statement, the system response is "
         for i in range(len(emotions_list)-1):
             response += f"'{emotions_list[i][0]}': {emotions_list[i][1]}, "
         response = response[:-2] + f" and '{emotions_list[-1][0]}': {emotions_list[-1][1]}."
