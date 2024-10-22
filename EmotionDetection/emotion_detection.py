b740a24e736b03862694f6d178e02342752479fd
rony gozes <ronygozes@gmail.com>
2024-10-21 20:37:51 -0400

    added dealing with empyu text

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,15 @@ def emotion_detector(text_to_analyse):
     input_json =  { "raw_document": { "text": text_to_analyse } }
     # print('im here')
     response = requests.post(url, json = input_json, headers=headers, timeout=2.50)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None, 
+            'fear': None, 
+            'joy': None, 
+            'sadness': None, 
+            'dominant_emotion': None
+        }
     response = json.loads(response.text)
     emotions = response['emotionPredictions'][0]['emotion']
     max_value = max(emotions.values())
@@ -16,4 +25,3 @@ def emotion_detector(text_to_analyse):
             emotions['dominant_emotion'] = key
             break
     return emotions
-    
--- server.py
+++ server.py
@@ -11,16 +11,19 @@ def index():
 def detect_emotions():
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
-    emotions_list = list(emotions.items())
-    for em in emotions_list:
-        if em[0] == 'dominant_emotion':
-            emotions_list.remove(em)
-            break
-    response = f"For the given statement, the system response is "
-    for i in range(len(emotions_list)-1):
-        response += f"'{emotions_list[i][0]}': {emotions_list[i][1]}, "
-    response = response[:-2] + f" and '{emotions_list[-1][0]}': {emotions_list[-1][1]}."
-    response += f" The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
+    if emotions['dominant_emotion'] is None:
+        response = 'Invalid text! Please try again!'
+    else:
+        emotions_list = list(emotions.items())
+        for em in emotions_list:
+            if em[0] == 'dominant_emotion':
+                emotions_list.remove(em)
+                break
+        response = f"For the given statement, the system response is "
+        for i in range(len(emotions_list)-1):
+            response += f"'{emotions_list[i][0]}': {emotions_list[i][1]}, "
+        response = response[:-2] + f" and '{emotions_list[-1][0]}': {emotions_list[-1][1]}."
+        response += f" The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
     return response
 
 if __name__ == '__main__':
