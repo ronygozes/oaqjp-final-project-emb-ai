a35abaa9ae5a3d54fe43a4a3051ffd483b514332
rony gozes <ronygozes@gmail.com>
2024-10-21 20:15:51 -0400

    fixed the server

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
-from flask import Flask, request
-
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/emotionDetector')
 def detect_emotions():
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
+    emotions_list = list(emotions.items())
+    for em in emotions_list:
+        if em[0] == 'dominant_emotion':
+            emotions_list.remove(em)
+            break
     response = f"For the given statement, the system response is "
-    for key, value in emotions:
-        if key != 'dominant_emotion':
-            response += f"'{key}': {value}, "
-    response = response[:-2] + f". The dominant emotion is {emotions['dominant_emotion']}."
+    for i in range(len(emotions_list)-1):
+        response += f"'{emotions_list[i][0]}': {emotions_list[i][1]}, "
+    response = response[:-2] + f" and '{emotions_list[-1][0]}': {emotions_list[-1][1]}."
+    response += f" The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
     return response
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
